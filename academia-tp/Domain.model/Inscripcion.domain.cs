01c53939918f0fff3d7d6dae2f35ceb3239dce4c
BrunoVitali <brunovitali33@gmail.com>
2025-10-07 14:01:01 -0300

    Todo el back de Inscripciones done, falta el form

---
+++ academia-tp/Domain.model/Inscripcion.domain.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.model
+{
+    public class Inscripcion
+    {
+        public int Id { get; set; }
+        public int Id_alumno { get; set; }
+
+        public int Id_curso { get; set; }
+
+        public string Condicion { get; set; }
+
+        public float? Nota { get; set; }
+
+    
+
+    public Inscripcion(int id_alumno, int id_curso, string condicion, float? nota)
+        {
+            Id_alumno = id_alumno;
+            Id_curso = id_curso;
+            Condicion = condicion;
+            Nota = nota;
+        }
+    } 
+
+}
