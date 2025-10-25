bb7eda68aabcba1b28ee693993d796ce67fc4a5d
pepicont <stefano.conti2004@gmail.com>
2025-10-25 13:03:33 -0300

    estado consistente login y protectedRoutes

--- academia-tp/BlazorWeb/Program.cs
+++ academia-tp/BlazorWeb/Program.cs
@@ -1,10 +1,13 @@
 using BlazorWeb.Components;
+using Domain.services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+builder.Services.AddScoped<AuthServiceScoped>(); //servicio auth singleton
 
 var app = builder.Build();
 
